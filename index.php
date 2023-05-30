9f71e20f31a8d34c432f3e42055107baab005c9d
Juan José Gamboa <juangamboaortiz@gmail.com>
2023-05-30 14:26:20 -0500

    feat: :sparkles: Sección 6
    Funciones de Salida

--- index.php
+++ index.php
@@ -2,7 +2,22 @@
 /**
  * !! 5. Introducción a PHP
  * * Es necesaria la estructura  <?php?> para indicar que es codigo PHP 
+ * 
+ * !! 6. Funciones de Salida
+ * * echo() se usa para imprimir en PHP
  */
+
+ echo "Hola Mundo";
+  
+ /**
+  * * Para crear una variable es necesario tener un signo de dolar
+  * * printf es similar a echo pero solo imprime una cadena de texto
+  * * sprintf devuelve la cadena formateada
+  */
+ $texto="Antonio";
+ printf("Hola %s", $texto);
+ 
+ $cadena=sprintf("Hola $",$texto);
 
  
  
