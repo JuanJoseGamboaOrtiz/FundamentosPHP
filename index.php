d2b99f89a1146deade6cc0faa4b81bc2e5844371
Juan Gamboa Ortiz <juangamboaortiz@gmail.com>
2023-05-31 16:08:04 -0500

    feat: :sparkles: Sección 10
    Arreglos asociativos y funciones para arreglos

--- index.php
+++ index.php
@@ -148,5 +148,115 @@
      */
 
      echo strpos($nombreCliente,'Pedro');
+
+    /**
+     !! 10 Arreglos,Arreglos asociativos y funciones para arreglos
+     ** Se pueden crear arrays de dos maneras usando la palabra reservada array o con corchetes
+     */
+
+     $Arreglo=array('Valor1','Valor2','Valor3');
     
+     $carrito=['Tablet','Televisión','Computadora'];
+
+     /**
+      ** Snippet para ver los contenidos de un array
+    */
+     echo "<pre>";
+     var_dump($carrito);
+     echo "</pre>";
+
+     /**
+      * * Acceder a un elemento del array
+      */
+    echo $carrito[0];
+
+    /**
+     * * Añadir un elemento mediante el indice
+     */
+    $carrito[3]='Nuevo Producto...';
+
+    /**
+     * * Añadir un nuevo elemento al final
+     */
+    array_push($carrito,'Audifonos');
+
+    /**
+     * * Añadir al inicio
+     */
+
+     array_unshift($carrito,'Smartwatch');
+
+    /**
+     * ? Arrays Asociativos
+     */
+
+     $arrayAsociativo= array(
+      "clave1"=>'valor1',
+      "clave2"=>'valor2',
+      "clave3"=>'valor3',
+     );
+
+    /**
+     * * Para imprimir valores de un array asociativo
+     */
+
+    echo $arrayAsociativo["clave1"];
+    echo $arrayAsociativo["clave2"];
+    echo $arrayAsociativo["clave3"];
+
+    /**
+     * * Recorrer un array asociativo con un foreach
+     */
+
+     foreach($arrayAsociativo as $clave=> $valor){
+      echo "Clave: " . $clave . ", Valor " . $valor;
+     }
+
+     /**
+      * * Ejemplo
+      */
+
+      $cliente=[
+        'nombre' =>'Juan',
+        'saldo' => 200,
+        'informacion'=>[
+          'tipo'=> 'premium',
+          'disponible'=>100
+        ]
+      ];
+
+      echo "<pre>";
+      var_dump($cliente);
+      echo '</pre>';
+
+      /**
+       * * Buscar elementos en un arreglo
+       */
+
+       $carrito=['Tablet','Computadora','Television'];
+
+       var_dump(in_array('Tablet',$carrito));
+       var_dump(in_array('Audifonos',$carrito));
+
+      /**
+       * * Ordenar elementos de un arreglo
+       */
+
+       $numeros= array(1,7,5,4,2,3,6,4);
+       sort($numeros); //de menor a mayor
+       rsort($numeros); // de mayor a menor
+       
+       echo "<pre>";
+      var_dump($numeros);
+      echo '</pre>';
+
+      /**
+       * * Ordenar arreglos asociativos
+       */
+
+       asort($cliente);
+       arsort($cliente);
+       ksort($cliente);
+       krsort($cliente);
+
 ?>
