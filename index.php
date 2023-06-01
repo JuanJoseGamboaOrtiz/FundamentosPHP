5975c3b8f5fb00bc0c98ae88afcdf94730ef4532
Juan jose Gamboa <Juan juangamboaortiz@gmail.com>
2023-06-01 17:23:42 -0500

    feat: :sparkles: Metodos de Array
    Un ejemplo de los metodos de array en php

--- index.php
+++ index.php
@@ -259,4 +259,163 @@
        ksort($cliente);
        krsort($cliente);
 
+       /**
+        * ? Metodos para los arreglos en PHP
+        */
+
+      /**
+      ** Intercambia las claves con sus valores correspondientes en un array. 
+      */
+      $array1=array(
+        'clave1' =>'key1',
+        'clave2' =>'key2',
+        'clave3' =>'key3',
+        'clave4' =>'key');
+
+      echo "Array Original";
+      echo '<pre>';
+      var_dump($array);
+      echo '/<pre>';
+      echo "Array con claves intercambiadas";
+      echo '<pre>';
+      var_dump(array_flip($array));
+      echo '/<pre>';
+      /**
+       * * array_fill() Rellena un Array con un valor especificado
+       */
+      $arrayEmpty=[];
+      echo '<pre>';
+      var_dump(array_fill(0,2,'Two Times'));
+      echo '/<pre>';
+      /**
+       * * array_filter(): Filtra los elementos de un array utilizando una función de devolución de llamada.
+       */
+      $numeros=[1,2,3,4,5,6,8,7,9,1,1,2,5,42,3];
+
+      function mayores2($var){
+        return $var>2;
+      }
+
+      var_dump(array_filter($numeros,'mayores2'));
+
+      /**
+       * * array_map(): Aplica una función a cada elemento de un array y devuelve un nuevo array con los resultados.
+       */
+
+       function por2($n){
+        return ($n*2);
+       }
+      
+      var_dump(array_map('por2',$numeros));
+      /**
+       ** array_reduce(): Reduce un array a un solo valor aplicando una función de devolución de llamada.
+       */
+
+       function suma($carry, $item){
+        $carry += $item;
+        return $carry;
+      } 
+
+      var_dump(array_reduce($numeros,'suma'));
+
+      /**
+       ** array_key_exists(): Comprueba si una clave existe en un array.
+      */
+
+       var_dump(array_key_exists('clave1',$array1));
+
+      /**
+      * *in_array(): Comprueba si un valor existe en un array.
+      */
+
+      var_dump(in_array('key1',$array1));
+
+      /**
+      * *array_rand(): Devuelve una o varias claves aleatorias de un array.
+      */
+
+      var_dump($aleatoria=array_rand($array));
+
+       /**
+      * *array_unique(): Elimina los valores duplicados de un array.
+      */
+
+      var_dump(array_unique($numeros));
+      /**
+      * *array_intersect(): Devuelve un array con los valores comunes a todos los arrays dados.
+      */
+
+      $array1 = array("a" => "green", "red", "blue");
+      $array2 = array("b" => "green", "yellow", "red");
+      $result = array_intersect($array1, $array2);
+      var_dump($result);
+
+      /**
+      * *array_diff(): Devuelve un array con los valores del primer array que no están presentes en los arrays adicionales.
+      */
+
+      $array1    = array("a" => "green", "red", "blue", "red","orange");
+      $array2    = array("b" => "green", "yellow", "red");
+      $resultado = array_diff($array1, $array2);
+
+      print_r($resultado);
+
+      /**
+      * *array_push(): Agrega uno o más elementos al final de un array.
+      */
+
+      var_dump(array_push($array1,"black"));
+
+       /**
+      * *array_pop(): Extrae y elimina el último elemento de un array.
+      */
+
+      var_dump($color=array_pop($array1));
+
+      /**
+      * *array_reverse(): Revierte el orden de los elementos en un array.
+      */
+
+      var_dump(array_reverse($array1));
+
+      /**
+      * *array_sum(): Devuelve la suma de todos los valores de un array numérico.
+      */
+
+      var_dump(array_sum($numeros));
+
+      /**
+      * *array_product(): Devuelve el producto de todos los valores de un array numérico.
+      */
+
+      var_dump(array_product($numeros));
+
+      /**
+      * *array_chunk(): Divide un array en fragmentos más pequeños.
+      */
+      var_dump(array_chunk($numeros,3,true));
+
+      /**
+      * *array_keys(): Devuelve todas las claves de un array.
+      */
+      var_dump(array_keys($array1));
+
+      /**
+      * *array_values(): Devuelve todos los valores de un array.
+      */
+
+      var_dump(array_values($array1));
+
+      /**
+      * *array_walk(): Aplica una función de devolución de llamada a cada elemento de un array.
+      */  
+      $fruits = array("d" => "lemon", "a" => "orange", "b" => "banana", "c" => "apple");
+      echo "Before ...:\n";
+      array_walk($fruits, 'test_print');
+
+
+
+
+
+      
 ?>
