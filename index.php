0b1aeb04308d51f029b522ab711a098b40558a46
Juan José Gamboa Ortiz <juangamboaortiz@gmail.com>
2023-06-13 20:24:04 -0500

    docs: :memo: PHP intermedio - POO definición y principales conceptos
    Se agrega la definicion de POO y los principales conceptos

--- index.php
+++ index.php
@@ -760,7 +760,35 @@
     <?php
      /**
      *    !! PHP intermedio
-     ** 
-     * */
+    **   !! 2.1 Programación orientada a objetos
+    **  La programación orientada a objetos (POO) es un paradigma de programación que se basa en el
+    **  concepto de "objetos". Los objetos son entidades que representan conceptos del mundo real y que
+    **  pueden tener propiedades (atributos) y realizar acciones (métodos).
+    **  En la programación orientada a objetos, los objetos son la base fundamental y se crean a partir de
+    **  clases. Una clase es una plantilla o un molde que define las propiedades y comportamientos que
+    **  tendrán los objetos que se creen a partir de ella
+
+    * ? Principales conceptos de la programación
+    **   Clase: Es una plantilla o definición que describe las características y comportamientos de los objetos que se pueden crear a partir de ella.
+
+    **   Objeto: Es una instancia de una clase. Representa un individuo o entidad específica y tiene sus propias propiedades y comportamientos.
+
+    **   Atributos: Son las propiedades o características de un objeto. Definen el estado de un objeto y se representan mediante variables en la clase.
+
+    **   Métodos: Son las acciones o comportamientos que un objeto puede realizar. Representan las operaciones que pueden realizarse con un objeto y se definen como funciones en la clase.
+    **   Encapsulación: Es el principio que establece que los atributos y métodos relacionados deben
+    **   agruparse en una clase para ocultar los detalles internos y exponer solo una interfaz pública.
+    **   Esto se logra mediante la especificación de niveles de acceso (público, privado, protegido)
+    **   para los atributos y métodos.
+
+    **   Herencia: Es un mecanismo que permite crear nuevas clases basadas en clases existentes. La
+    **   clase que se utiliza como base se denomina "clase padre" o "superclase", y la clase que se
+    **   deriva se llama "clase hija" o "subclase". La herencia permite la reutilización de código y la
+    **   creación de jerarquías de clases.
+    
+    **   Polimorfismo: Es la capacidad de un objeto de tomar diferentes formas o comportarse de
+    **   diferentes maneras según el contexto. Permite utilizar una interfaz común para objetos de
+    **   diferentes clases y proporciona flexibilidad y extensibilidad en el diseño de programas.
+    * */
 
      
